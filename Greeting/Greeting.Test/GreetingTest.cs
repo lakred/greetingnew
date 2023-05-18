74a81b241321cee2aa6230f5dc446c781616d282
mlakred <115311821+lakred@users.noreply.github.com>
2023-05-18 15:16:49 +0200

    add requirement from 1 to 4

---
+++ Greeting/Greeting.Test/GreetingTest.cs
@@ -0,0 +1,35 @@
+namespace Greeting.Test
+{
+    public class GreetingTest
+    {
+        private readonly Greeting _sut;
+        public GreetingTest()
+        {
+            _sut = new Greeting();
+        }
+        [Fact]
+        public void SimpleGreet()
+        {
+            var result= _sut.Greet("Bob");
+            Assert.Equal("Hello, Bob.", result);
+        }
+        [Fact]
+        public void EmptyGreet()
+        {
+            var result = _sut.Greet(null);
+            Assert.Equal("Hello, my friend.", result);
+        }
+        [Fact]
+        public void UpperGreet()
+        {
+            var result = _sut.Greet("JERRY");
+            Assert.Equal("HELLO, JERRY!", result);
+        }
+        [Fact]
+        public void TwoNamesGreet()
+        {
+            var result = _sut.Greet("Jill", "Jane");
+            Assert.Equal("Hello, Jill and Jane.", result);
+        }
+    }
+}
---
+++ Greeting/Greeting/Greeting.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greeting
+{
+    public class Greeting
+    {
+        public string Greet(params string[] names)
+        {
+            if (names == null)
+            {
+                return "Hello, my friend.";
+            }
+            else if (names.Length == 1)
+            {
+                return names[0] == names[0].ToUpper()
+                    ? $"HELLO, {names[0]}!"
+                    : $"Hello, {names[0]}.";
+            }
+                return $"Hello, {names[0]} and {names[1]}.";
+            
+
+        }
+    }
+}
