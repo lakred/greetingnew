ec344e90f6ba9b911692acc952f98dbe18e25521
mohamedlakred <lakred@outlook.it>
2023-05-19 12:57:39 +0200

    Second version of final refactory

--- Greeting/Greeting/Greeting.cs
+++ Greeting/Greeting/Greeting.cs
@@ -6,12 +6,10 @@ using System.Threading.Tasks;
 using System.Xml.Linq;
 using System.Text.RegularExpressions;
 
-
 namespace Greeting
 {
     public class Greeting 
     {
-
         public string Greet(params string[] names)
         {
             if (names == null)
@@ -32,111 +30,43 @@ namespace Greeting
                 {
                     word.AddRange(isComma && !isApice ? name.Split(',').ToList() : new List<string> { name });
                 }
-
                 if (!isComma && names.Length == 2)
                 {
                     return $"Hello, {names[0]} and {names[1]}.";
                 }
-
                 string[] namesWord = word.ToArray();
-
                 namesWord = namesWord.Select(name =>
                 {
                     return (name.Contains("\"")) ? Regex.Replace(name, "\"", "") : Regex.Replace(name, @"\s", "");
                 }
                 ).ToArray();
 
-
-
-
-
                 bool hasUpper = word.Any(name =>name==name.ToUpper());
                 if (!hasUpper)
                 {
-                    StringBuilder greet = new StringBuilder();
-                    greet.Append("Hello, ");
-
-                    for (int i = 0; i < namesWord.Length; i++)
-                    {
-
-                        if (i == namesWord.Length - 1)
-                        {
-                            greet.Append("and ");
-                            greet.Append(namesWord[i]);
-                            greet.Append(".");
-                        }
-                        else if (i == namesWord.Length - 2)
-                        {
-                            greet.Append(namesWord[i]);
-                            greet.Append(isApice ? " " : ", ");
-                        }
-                        else
-                        {
-                            greet.Append(namesWord[i]);
-                            greet.Append(", ");
-                        }
-                    }
-                    return greet.ToString();
+                    string lastSeparator = (namesWord.Length > 1) ? (isApice ? " " : ", ") : "";
+                    string greeting = $"Hello, {string.Join(", ", namesWord[0..^1])}{lastSeparator}and {namesWord[^1]}.";
+                    return greeting;
                 }
                 else
                 {
-                    int CountUpper = namesWord.Count(name => name == name.ToUpper());
-                    string[] namesUpper = new string[CountUpper];
-                    int m = 0;
-
-                    for (int i=0; i < namesWord.Length; i++)
-                    {
-                        if (namesWord[i] == namesWord[i].ToUpper())
-                        {
-                            namesUpper[m] = namesWord[i];
-                            m++;
-                        }
-                    }
-                    string[] namesLower = new string[namesWord.Length- CountUpper];
-                    int n = 0;
-                    for (int i = 0; i < namesWord.Length; i++)
-                    {
-                        if (names[i] != namesWord[i].ToUpper())
-                        {
-                           
-                            namesLower[n] = namesWord[i];
-                            n++;
-                        }
-                    }
+                    string[] namesUpper = namesWord.Where(name => name == name.ToUpper()).ToArray();
+                    string[] namesLower = namesWord.Where(name => name != name.ToUpper()).ToArray();
                     StringBuilder greet = new StringBuilder();
                     greet.Append("Hello, ");
                     for (int i = 0; i < namesLower.Length; i++)
                     {
-
-                        if (i == namesLower.Length - 1)
-                        {
-                            greet.Append("and ");
-                            greet.Append(namesLower[i]);
-                            greet.Append(".");
-                        }
-                        else
-                        {
-                            greet.Append(namesLower[i]);
-                            greet.Append((namesLower.Length > 2)? ", ": " ");
-                         }
+                        greet.Append((i == namesLower.Length - 1)
+                            ? "and " + namesLower[i] + "."
+                            : namesLower[i] + ((namesLower.Length > 2) ? ", " : " ")
+                            );
                     }
                     greet.Append(" AND HELLO ");
-                    for (int i = 0; i < namesUpper.Length; i++)
+                    if (namesUpper.Length > 0)
                     {
-                        if (i == namesUpper.Length-1&&i>=1)
-                        {
-                            greet.Append("AND ");
-                            greet.Append(namesUpper[i]);
-                        }
-                        else if (i == namesUpper.Length - 1 && i < 1)
-                        {
-                            greet.Append(namesUpper[i]);
-                        }
-                        else
-                        {
-                            greet.Append(namesUpper[i]);
-                            greet.Append(", ");
-                        }
+                        greet.Append(string.Join(", ", namesUpper.Take(namesUpper.Length - 1)));
+                        greet.Append(namesUpper.Length > 1 ? " AND " : "");
+                        greet.Append(namesUpper.Last());
                     }
                     greet.Append("!");
                     return greet.ToString();
